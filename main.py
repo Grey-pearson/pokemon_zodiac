612708e39ed0d15ee9ddf40c640306123bbb7030
grey <greytpearson@gmail.com>
2024-10-17 14:23:33 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,12 @@
 # searches csv for pokemon
 
 birth_day = int(input('what day were you born on?: '))
-birth_month = int(input('what month (number) where you born in?: '))
+age = int(input('whats your age?: '))
 
-def pokemon_number(birth_day, birth_month):
-    birth_number = birth_month + birth_day
+def pokemon_number(birth_day, age):
+    birth_number = age + birth_day
     pokemon_number = birth_number * 4
     return pokemon_number
 
-pokemon = pokemon_number(birth_day, birth_month)
+pokemon = pokemon_number(birth_day, age)
 print(pokemon)
