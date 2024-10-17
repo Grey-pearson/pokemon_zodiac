837c199485cd539d7922f23c728363190925e645
grey <greytpearson@gmail.com>
2024-10-17 14:21:51 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,13 @@
 # multiplies num by 4
 # searches csv for pokemon
 
-birth_day = input('what day were you born on?: ')
+birth_day = int(input('what day were you born on?: '))
+birth_month = int(input('what month (number) where you born in?: '))
+
+def pokemon_number(birth_day, birth_month):
+    birth_number = birth_month + birth_day
+    pokemon_number = birth_number * 4
+    return pokemon_number
+
+pokemon = pokemon_number(birth_day, birth_month)
+print(pokemon)
