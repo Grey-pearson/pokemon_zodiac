17093ec98c0fe19162dca5103ade9434a027f5a1
grey <greytpearson@gmail.com>
2024-10-17 14:32:15 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,12 @@
 birth_day = int(input('what day were you born on?: '))
 age = int(input('whats your age?: '))
 
+age_range = bool(input('if you want just your current age hit 0, if you want to know pokemon for 1-80 press 1'))
+print(age_range)
+
 def pokemon_number(birth_day, age):
-    birth_number = age + birth_day
-    pokemon_number = birth_number * 4
+    age_day = age + birth_day
+    pokemon_number = age_day * 4
     return pokemon_number
 
 pokemon = pokemon_number(birth_day, age)
